a23f51379ca57b2fc8768c575444f8cef8007c87
Dylan66 <dylannganga@gmail.com>
2025-01-20 11:37:58 +0300

    variables

---
+++ dev3.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+ func main () {
+	averageOpenRate, displayMessage := .23, " is the average messagerate"
+	fmt.Println("The credentials are:", averageOpenRate,displayMessage)
+ }
