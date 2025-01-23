0d213ff7a7cffbaf3172d99458ce658f6aa4171f
Dylan66 <dylannganga@gmail.com>
2025-01-23 10:18:33 +0300

    structs

--- structs.go
+++ structs.go
@@ -9,15 +9,49 @@ type carType struct{
     year int
     model string
     color string
+    frontWheel wheel
+    backWheel wheel
 
 }
+type wheel struct
+{
+    radius int
+    material string
+}
+func checkCar(ccarType carType) bool {
+    if ccarType.year==0 {
+    return false
+    }
+
+    if ccarType.model==""{
+        return false
+    }
+    if ccarType.frontWheel.radius==0 {
+        return false
+    }
+
+    return true
+
+
+}
+
+//function to test the struct
 func test( c carType){
     fmt.Println("The brand of the car is", c.toyota, "and its color is", c.color)
     fmt.Println("The model of the car is", c.model, "and its year is", c.year)
     fmt.Printf("The car's manufacture year is %d and its model is %s", c.year, c.model)
+    fmt.Println("The front wheel radius is", c.frontWheel.radius, "and its material is", c.frontWheel.material)
 }
 func main(){
-    test(carType{"mark x", 2019, "mark x evolution", "black"})
+    car:= carType{"mark x", 0, "mark x evolution", "black", wheel{20, "rubber"}, wheel{20, "rubber"}}
+   
+   
+    if checkCar(car) {
+        fmt.Println("The car is valid")
+        test(car)
+    } else {
+        fmt.Println("The car is not valid")
+    }
 
 
 }
