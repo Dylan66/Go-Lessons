225b1c153417c622bdeb186cea63f89ca3e8eb4a
Dylan66 <dylannganga@gmail.com>
2025-01-24 13:14:04 +0300

    methods structs

--- methods.go
+++ methods.go
@@ -1,1 +1,19 @@
-p
+package main
+
+import "fmt"
+
+type triangle struct {
+	height int
+	base int
+
+}
+
+func (tri triangle) area () float64 {
+	return float64(tri.base*tri.height)*(0.5)
+
+}
+
+func main() {
+	tri := triangle{height: 18, base: 20}
+	fmt.Printf("The area of the triangle is %.2f", tri.area())
+}
